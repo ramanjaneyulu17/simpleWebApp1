30c3a29070fb7d1ebacc52f69aca4d1e43705163
ramanjaneyulu17 <ramanjaneyulu17.m@gmail.com>
2024-12-10 19:37:42 +0530

    demo controller

--- src/main/java/com/example/simpleWebApp1/controller/DemoController.java
+++ src/main/java/com/example/simpleWebApp1/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.simpleWebApp1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
